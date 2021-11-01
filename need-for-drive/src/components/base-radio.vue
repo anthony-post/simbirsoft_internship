9aa78ca51695fe2309d46ec87e9b3b63b5469c12
anthony-post <anthony-post@HOTMAIL.COM>
2021-11-01 21:17:47 +0300

    v-03

---
+++ need-for-drive/src/components/base-radio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="radio__item">
+    <input class="radio__input" type="radio" name="cars" />
+    <label for="cars" v-if="label">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
--- need-for-drive/src/components/order-model.vue
+++ need-for-drive/src/components/order-model.vue
@@ -1,17 +1,81 @@
 <!--TO DO-->
 <template>
   <div>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae reiciendis
-      nostrum fugit in deleniti aut. Exercitationem debitis sequi similique.
-      Repellat nesciunt numquam odit facere cupiditate cum dolorem corporis
-      libero hic?
-    </p>
+    <div class="radio-list">
+      <BaseRadio label="Все модели" />
+      <BaseRadio label="Эконом" />
+      <BaseRadio label="Премиум" />
+    </div>
+    <ul class="cars-list">
+      <li class="cars__item" v-for="car in cars" :key="car.id">
+        <p class="cars__model">{{ car.model }}</p>
+        <p class="cars__price">{{ car.pricemin }} - {{ car.pricemin }}</p>
+        <!-- <img :src="car.img" alt="автомобиль" /> -->
+        <img
+          class="cars__img"
+          src="../assets/car_elantra.jpg"
+          alt="автомобиль"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import BaseRadio from "@/components/base-radio.vue";
+
 export default {
   name: "order-model",
+  components: {
+    BaseRadio,
+  },
+  data() {
+    return {
+      cars: [
+        {
+          id: "1",
+          img: "../assets/car_elantra.jpg",
+          model: "ELANTRA",
+          pricemin: "12 000",
+          pricemax: "25 000",
+        },
+        {
+          id: "2",
+          img: "../assets/car_i30n.jpg",
+          model: "i30 N",
+          pricemin: "18 000",
+          pricemax: "32 000",
+        },
+        {
+          id: "3",
+          img: "../assets/car_creta.jpg",
+          model: "CRETA",
+          pricemin: "12 000",
+          pricemax: "25 000",
+        },
+        {
+          id: "4",
+          img: "../assets/car_sonata.jpg",
+          model: "SONATA",
+          pricemin: "18 000",
+          pricemax: "32 000",
+        },
+        {
+          id: "5",
+          img: "../assets/car_elantra.jpg",
+          model: "ELANTRA",
+          pricemin: "12 000",
+          pricemax: "25 000",
+        },
+        {
+          id: "6",
+          img: "../assets/car_i30n.jpg",
+          model: "i30 N",
+          pricemin: "18 000",
+          pricemax: "32 000",
+        },
+      ],
+    };
+  },
 };
 </script>
